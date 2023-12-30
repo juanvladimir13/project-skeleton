6d7f2bc5150dd44d0aadeb850deccd1760c6bfb5
Juan Vladimir <juanvladimir13@gmail.com>
2023-12-30 19:49:40 -0400

    initials commits

---
+++ .env-dev.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @author juanvladimir13 <juanvladimir13@gmail.com>
+ * @see https://github.com/juanvladimir13
+ */
+
+return [
+    'username' => 'juanvladimir13',
+];
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @author juanvladimir13 <juanvladimir13@gmail.com>
+ * @see https://github.com/juanvladimir13
+ */
+
+require '../vendor/autoload.php';
+
+$config = require '../.env.php';
+echo $config['username'];
